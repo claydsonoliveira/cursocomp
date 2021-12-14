318359b89e1d5e265b58a813725479e3d7101572
Claydson Oliveira <claydsonoliveira@gmail.com>
2021-12-14 00:22:48 -0300

    Eureka server nao visualiza os outros microservices

--- course/src/main/java/com/academy/course/CourseApplication.java
+++ course/src/main/java/com/academy/course/CourseApplication.java
@@ -3,9 +3,11 @@ package com.academy.course;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EntityScan({"com.academy.core.model"})
 @EnableJpaRepositories({"com.academy.core.repository"})
 public class CourseApplication {
