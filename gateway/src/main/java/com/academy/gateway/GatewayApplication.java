3fe97d15da328e2a7958425ea8d0937c73037505
Claydson Oliveira <claydsonoliveira@gmail.com>
2021-12-14 23:11:33 -0300

    zuul

--- gateway/src/main/java/com/academy/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/academy/gateway/GatewayApplication.java
@@ -5,13 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-<<<<<<< HEAD
-@EnableZuulProxy
-@EnableEurekaClient
-=======
 @EnableEurekaClient
 @EnableZuulProxy
->>>>>>> 086fb90ef8e18db4bd8582362b6d780edd4b11cb
 @SpringBootApplication
 public class GatewayApplication {
 	public static void main(String[] args) {
